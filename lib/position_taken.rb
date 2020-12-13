af18fb93304c6e2787ecba0258bbfbd4dbac790c
charlon tamayo <charlont@gmail.com>
2020-12-13 16:11:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index_number == " " || index_number == ""
+  if index == " " || index == ""
    "false"
   end
 end
